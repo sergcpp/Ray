13252314aad8373a5f93fcf935ecc8841e44f3cb
Sergey Yablokov <yablokovsergey1@gmail.com>
2019-08-04 22:20:46 +0200

    Fix tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -8,14 +8,11 @@ void test_mesh_lights();
 
 int main() {
     test_atlas();
-#if 0
     test_simd();
     test_primary_ray_gen();
 #ifndef _DEBUG
     test_mesh_lights();
 #endif
-#endif
-
     puts("OK");
 }
 
