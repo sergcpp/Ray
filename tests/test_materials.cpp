9b49fd42c37fd04575242309cf05e85ceae655ab
Sergey Yablokov <yablokovsergey1@gmail.com>
2023-01-04 19:20:42 +0100

    Add missing image to list

--- tests/test_materials.cpp
+++ tests/test_materials.cpp
@@ -916,7 +916,7 @@ void assemble_material_test_images(const char *arch_list[]) {
         {"trans_mat0", "trans_mat1", "trans_mat2", "trans_mat3", "trans_mat4"},
         {"trans_mat5", "trans_mat6", "trans_mat7", "trans_mat8", "trans_mat9"},
         {"alpha_mat0", "alpha_mat1", "alpha_mat2", "alpha_mat3"},
-        {"complex_mat0", "complex_mat1", "complex_mat2", "complex_mat3"},
+        {"complex_mat0", "complex_mat1", "complex_mat2", "complex_mat3", "complex_mat4"},
         {"complex_mat5", "complex_mat5_mesh_lights", "complex_mat5_sphere_light", "complex_mat5_sun_light",
          "complex_mat5_hdr_light"},
         {"complex_mat6", "complex_mat6_mesh_lights", "complex_mat6_sphere_light", "complex_mat6_sun_light",
