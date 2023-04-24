3ac56247a9732dbdb05bb3839667a9b4739c64d1
Sergey Yablokov <yablokovsergey1@gmail.com>
2023-04-24 17:02:37 +0200

    Add malloc argument check  (to supress warning)

--- internal/simd/aligned_allocator.h
+++ internal/simd/aligned_allocator.h
@@ -14,9 +14,15 @@ inline void *aligned_malloc(size_t size, size_t alignment) {
     while (alignment < sizeof(void *)) {
         alignment *= 2;
     }
-    size_t space = size + (alignment - 1);
+    size_t space = size + (alignment - 1) + sizeof(void *);
+    if (space > PTRDIFF_MAX) {
+        return nullptr;
+    }
 
-    void *ptr = malloc(space + sizeof(void *));
+    void *ptr = malloc(space);
+    if (!ptr) {
+        return nullptr;
+    }
     void *original_ptr = ptr;
 
     char *ptr_bytes = static_cast<char *>(ptr);
