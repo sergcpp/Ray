d395eac9ce9d2fe0493c6758a7dc5fd895dc3f70
Sergey Yablokov <yablokovsergey1@gmail.com>
2023-05-30 15:33:26 +0200

    Use min dimension for mip counting

--- internal/SamplingParams.cpp
+++ internal/SamplingParams.cpp
@@ -5,10 +5,10 @@
 int Ray::CalcMipCount(const int w, const int h, const int min_res, const eTexFilter filter) {
     int mip_count = 0;
     if (filter == eTexFilter::Trilinear || filter == eTexFilter::Bilinear) {
-        int max_dim = std::max(w, h);
+        int min_dim = std::min(w, h);
         do {
             mip_count++;
-        } while ((max_dim /= 2) >= min_res);
+        } while ((min_dim /= 2) >= min_res);
     } else {
         mip_count = 1;
     }
