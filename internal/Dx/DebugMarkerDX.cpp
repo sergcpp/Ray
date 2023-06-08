0fa657a0a0b35e3e9abbcf9ee6bcb4c48eb46362
Sergey Yablokov <yablokovsergey1@gmail.com>
2023-06-08 22:25:12 +0200

    Basic PIX integration

--- internal/Dx/DebugMarkerDX.cpp
+++ internal/Dx/DebugMarkerDX.cpp
@@ -11,9 +11,25 @@
 #endif
 #include <d3d12.h>
 
+#include "../../Config.h"
+
+#ifdef ENABLE_PIX
+#include <WinPixEventRuntime/pix3.h>
+#endif
+
 Ray::Dx::DebugMarker::DebugMarker(void *_cmd_buf, const char *name) : cmd_buf_((ID3D12GraphicsCommandList *)_cmd_buf) {
+#ifdef ENABLE_PIX
+    PIXBeginEvent(cmd_buf_, 0, name);
+#else
     std::wstring wstr(name, name + strlen(name));
     cmd_buf_->BeginEvent(0, wstr.c_str(), UINT(wstr.length() * sizeof(wchar_t)));
+#endif
 }
 
-Ray::Dx::DebugMarker::~DebugMarker() { cmd_buf_->EndEvent(); }
+Ray::Dx::DebugMarker::~DebugMarker() {
+#ifdef ENABLE_PIX
+    PIXEndEvent(cmd_buf_);
+#else
+    cmd_buf_->EndEvent();
+#endif
+}
