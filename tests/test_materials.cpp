8a044a90b5d19fbbbb36c8c218f84849d3337e6d
Sergey Yablokov <yablokovsergey1@gmail.com>
2023-06-17 00:50:09 +0200

    Adjust threshold  [ci skip]

--- tests/test_materials.cpp
+++ tests/test_materials.cpp
@@ -1517,7 +1517,7 @@ void test_complex_mat5_spot_light(const char *arch_list[], const char *preferred
 
 void test_complex_mat5_sun_light(const char *arch_list[], const char *preferred_device) {
     const int SampleCount = 991; // 541;
-    const int PixThres = 2061;
+    const int PixThres = 2064;
 
     Ray::principled_mat_desc_t metal_mat_desc;
     metal_mat_desc.base_texture = Ray::TextureHandle{0};
