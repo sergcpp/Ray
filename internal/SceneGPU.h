f9f9637fe840add5f3500327c63fa0a9276da0bf
Sergey Yablokov <yablokovsergey1@gmail.com>
2023-10-20 20:45:37 +0200

    Fix uninitialized envlight index

--- internal/SceneGPU.h
+++ internal/SceneGPU.h
@@ -1463,6 +1463,7 @@ inline void Ray::NS::Scene::Finalize() {
     }
     env_map_qtree_ = {};
     env_.qtree_levels = 0;
+    env_.light_index = 0xffffffff;
 
     if (env_.env_map != InvalidTextureHandle._index &&
         (env_.env_map == PhysicalSkyTexture._index || env_.env_map == physical_sky_texture_._index)) {
@@ -1492,6 +1493,7 @@ inline void Ray::NS::Scene::Finalize() {
 
             const std::pair<uint32_t, uint32_t> li = lights_.push(l);
             env_map_light_ = LightHandle{li.first, li.second};
+            env_.light_index = env_map_light_._index;
             li_indices_.PushBack(env_map_light_._index);
             visible_lights_.PushBack(env_map_light_._index);
         }
