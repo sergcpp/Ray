3037662156292a74ab817e98b3974c7d2a4bd13c
Sergey Yablokov <yablokovsergey1@gmail.com>
2023-11-11 21:33:50 +0100

    Adjust thresholds

--- tests/test_materials.cpp
+++ tests/test_materials.cpp
@@ -1654,7 +1654,7 @@ void test_complex_mat5_spot_light(const char *arch_list[], const char *preferred
 void test_complex_mat5_sun_light(const char *arch_list[], const char *preferred_device) {
     const int SampleCount = 8;
     const double MinPSNR = 23.0;
-    const int PixThres = 6039;
+    const int PixThres = 6041;
 
     Ray::principled_mat_desc_t metal_mat_desc;
     metal_mat_desc.base_texture = Ray::TextureHandle{0};
