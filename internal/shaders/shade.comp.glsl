4ec4af1b7fa90685c38f451ca18386a8f4d43cef
Sergey Yablokov <yablokovsergey1@gmail.com>
2024-01-26 21:24:01 +0100

    Minor refactoring

--- internal/shaders/shade.comp.glsl
+++ internal/shaders/shade.comp.glsl
@@ -422,9 +422,9 @@ vec3 offset_ray(vec3 p, vec3 n) {
                           intBitsToFloat(floatBitsToInt(p[1]) + ((p[1] < 0.0) ? -of_i[1] : of_i[1])),
                           intBitsToFloat(floatBitsToInt(p[2]) + ((p[2] < 0.0) ? -of_i[2] : of_i[2])));
 
-    return vec3(abs(p[0]) < Origin ? (p[0] + FloatScale * n[0]) : p_i[0],
-                abs(p[1]) < Origin ? (p[1] + FloatScale * n[1]) : p_i[1],
-                abs(p[2]) < Origin ? (p[2] + FloatScale * n[2]) : p_i[2]);
+    return vec3(abs(p[0]) < Origin ? fma(FloatScale, n[0], p[0]) : p_i[0],
+                abs(p[1]) < Origin ? fma(FloatScale, n[1], p[1]) : p_i[1],
+                abs(p[2]) < Origin ? fma(FloatScale, n[2], p[2]) : p_i[2]);
 }
 
 // http://jcgt.org/published/0007/04/01/paper.pdf
