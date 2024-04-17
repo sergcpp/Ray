a950a615f78cd245a3f7c7b158f111482443a256
Sergey Yablokov <yablokovsergey1@gmail.com>
2024-04-17 18:03:42 +0200

    Properly clamp cached radiance

--- internal/ShadeRef.cpp
+++ internal/ShadeRef.cpp
@@ -1347,11 +1347,15 @@ Ray::color_rgba_t Ray::Ref::ShadeSurface(const pass_settings_t &ps, const float 
                 find_entry(sc.spatial_cache_entries, surf.P, surf.plane_N, sc.spatial_cache_grid);
             if (cache_entry != HASH_GRID_INVALID_CACHE_ENTRY) {
                 const packed_cache_voxel_t &voxel = sc.spatial_cache_voxels[cache_entry];
-                cache_voxel_t unpacked = unpack_voxel_data(voxel);
+                const cache_voxel_t unpacked = unpack_voxel_data(voxel);
                 if (unpacked.sample_count >= RAD_CACHE_SAMPLE_COUNT_MIN) {
                     fvec4 color = make_fvec3(unpacked.radiance) / float(unpacked.sample_count);
                     color /= sc.spatial_cache_grid.exposure;
                     color *= fvec4{ray.c[0], ray.c[1], ray.c[2], 0.0f};
+                    const float sum = hsum(color);
+                    if (sum > limits[1]) {
+                        color *= (limits[1] / sum);
+                    }
                     return color_rgba_t{color.get<0>(), color.get<1>(), color.get<2>(), color.get<3>()};
                 }
             }
